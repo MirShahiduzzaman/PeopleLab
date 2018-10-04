4b86ae671649aa403ed5eb58cae42d9acd314b57
Afaq Anwar <anwarafaq@outlook.com>
2018-10-04 10:15:58 -0400

    Created Teacher Class, and Runner Class.
    Signed-off-by: Afaq Anwar <anwarafaq@outlook.com>

---
+++ src/PeopleLab/Runner.java
@@ -0,0 +1,10 @@
+package PeopleLab;
+
+public class Runner {
+    String[] firstNames = {"Mary", "Mir", "Afaq", "Jacky", "Bob"};
+    String[] familyNames = {"Walker", "Sha", "Anwar", "Fang", "Billy"};
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/PeopleLab/Teacher.java
@@ -0,0 +1,21 @@
+package PeopleLab;
+
+public class Teacher extends Person {
+    private String Subject;
+    private String Title;
+
+    public Teacher(String firstName, String familyName, String Subject, String Title) {
+        super(firstName, familyName);
+        this.Subject = Subject;
+        this.Title = Title;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    @Override
+    public String toString() {
+        return Title;
+    }
+}
