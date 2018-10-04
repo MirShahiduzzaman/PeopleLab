5358ffbac81b37eefaa7c0b7838ce29855e208b0
BT_1N3_20@bktech.local <BT_1N3_20@bktech.local>
2018-10-04 09:35:45 -0400

    Initial commit

---
+++ src/PeopleLab/Person.java
@@ -0,0 +1,50 @@
+package PeopleLab;
+
+public abstract class Person
+{
+    private String firstName;
+    private String familyName;
+
+    public Person(String firstName, String familyName)
+    {
+        this.firstName = firstName;
+        this.familyName = familyName;
+    }
+
+   public void setFirstName(String firstName)
+   {
+       this.firstName = firstName;
+   }
+
+   public void setFamilyName(String familyName)
+   {
+       this.familyName = familyName;
+   }
+
+
+
+    public String getFirstName()
+    {
+        return(firstName);
+    }
+
+    public String getFamilyName()
+    {
+        return(familyName);
+    }
+
+    public abstract String toString();
+
+    public boolean equals(Person person)
+    {
+        if(this.firstName.equals(person.getFirstName())&& this.familyName.equals(person.getFamilyName()))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
