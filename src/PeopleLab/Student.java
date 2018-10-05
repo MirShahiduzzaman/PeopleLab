f3d3d75d7ed813fb9606554dd1c4ef4f210f9a61
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-10-05 09:14:01 -0400

    Added Student class
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

---
+++ src/PeopleLab/Student.java
@@ -0,0 +1,43 @@
+package PeopleLab;
+
+public abstract class Student extends Person
+{
+    private double GPA;
+    private double mathScore;
+    private double elaScore;
+
+
+    public Student(String firstName, String familyName, double GPA, double mathScore, double elaScore)
+    {
+        super(firstName,familyName);
+
+        this.GPA = GPA;
+        this.mathScore = mathScore;
+        this.elaScore = elaScore;
+    }
+
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+
+    public double getMathScore() {
+        return mathScore;
+    }
+
+    public void setMathScore(double mathScore) {
+        this.mathScore = mathScore;
+    }
+
+    public double getElaScore() {
+        return elaScore;
+    }
+
+    public void setElaScore(double elaScore) {
+        this.elaScore = elaScore;
+    }
+
+    public String toString()
+    {
+        return(getFamilyName() + ", " + getFirstName());
+    }
+}
